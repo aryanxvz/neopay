2a09831e50b200abb76a131a4d8ecbdcd1f0fce2
Aryan Mane <aryanmane7916@gmail.com>
2024-06-17 12:51:32 +0530

    added redirect after signup

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,23 +1,14 @@
-"use client"
 
-import { SessionProvider, signIn, signOut, useSession } from "next-auth/react";
-import { Appbar } from "../../../packages/ui/src/Appbar";
+import { getServerSession } from "next-auth";
+import { redirect } from 'next/navigation'
+import { authOptions } from "./lib/auth";
 
-// Create a wrapper component to use SessionProvider
-function PageContent() {
-  const session = useSession();
-
-  return (
-    <div>
-      <Appbar onSignin={signIn} onSignout={signOut} user={session.data?.user} />
-    </div>
-  );
-}
-
-export default function Page() {
-  return (
-    <SessionProvider>
-      <PageContent />
-    </SessionProvider>
-  );
+export default async function Page() {
+    const session = await getServerSession(authOptions);
+    
+    if (session?.user) {
+        redirect('/dashboard')
+    } else {
+        redirect('/api/auth/signin')
+    }
 }
