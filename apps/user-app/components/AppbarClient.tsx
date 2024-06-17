ee788ccbaee5500c81a8c1f05f45b2c01d44723a
Aryan Mane <aryanmane7916@gmail.com>
2024-06-17 12:47:43 +0530

    completed dashboard layout and few changes in appbar

--- apps/user-app/components/AppbarClient.tsx
+++ apps/user-app/components/AppbarClient.tsx
@@ -1,19 +1,31 @@
-"use client"
-import { signIn, signOut, useSession } from "next-auth/react";
+"use client";
+import { SessionProvider, signIn, signOut, useSession } from "next-auth/react";
 import { Appbar } from "../../../packages/ui/src/Appbar";
 import { useRouter } from "next/navigation";
 import React from "react";
 
-export function AppbarClient() {
+function AppbarClientContent() {
   const session = useSession();
   const router = useRouter();
 
   return (
-   <div>
-      <Appbar onSignin={signIn} onSignout={async () => {
-        await signOut()
-        router.push("/api/auth/signin")
-      }} user={session.data?.user} />
-   </div>
+    <div>
+      <Appbar
+        onSignin={signIn}
+        onSignout={async () => {
+          await signOut();
+          router.push("/api/auth/signin");
+        }}
+        user={session.data?.user}
+      />
+    </div>
+  );
+}
+
+export function AppbarClient() {
+  return (
+    <SessionProvider>
+      <AppbarClientContent />
+    </SessionProvider>
   );
 }
