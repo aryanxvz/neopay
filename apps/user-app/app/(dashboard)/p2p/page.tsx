a46855ee57ddb5a09d1b368686191884eb474302
Aryan Mane <aryanmane7916@gmail.com>
2024-06-19 19:29:26 +0530

    p2p transactions completed

--- apps/user-app/app/(dashboard)/p2p/page.tsx
+++ apps/user-app/app/(dashboard)/p2p/page.tsx
@@ -1,8 +1,9 @@
 
 import React from "react";
+import { SendCard } from "../../../components/SendCard";
 
 export default function() {
-    return <div>
-        P2P
+    return <div className="w-full">
+        <SendCard />
     </div>
 }
